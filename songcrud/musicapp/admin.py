2d16e12e26290c8727f675cecdfdab560175d14e
Damilola305 <111053592+Damilola305@users.noreply.github.com>
2022-10-29 09:25:46 +0100

    songcrud musicapp

---
+++ songcrud/musicapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Song, Lyric, Artiste
+
+# Register your models here.
+
+
+# Register your models here.
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
+
